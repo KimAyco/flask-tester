1a448b0fb7c7dcda9f466e31fcaab3213007ce98
KimAyco <kimayco1@gmail.com>
2025-05-23 17:06:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ CORS(app, resources={r"/*": {"origins": [
     "http://127.0.0.1:5500",
     "https://kimayco.github.io",
     "https://kimayco.github.io/mediapipetest1"
+    "http://localhost/capstone/signspeak2.6/translate.php"
 ]}})
   # Allow requests from your local HTML page
 
