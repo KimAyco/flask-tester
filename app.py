dc3c71a223fbb982541e0a349a64383b816a7bf3
KimAyco <kimayco1@gmail.com>
2025-09-07 18:32:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ CORS(app, resources={r"/*": {"origins": [
   # Allow requests from your local HTML page
 
 # Label map
-label_map = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: 'hello', 11: 'j', 12: 'z'}  # Adjust as needed
+label_map = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'A': 10, 'B': 11, 'Big': 12, 'Book': 13, 'C': 14, 'D': 15, 'Drink': 16, 'E': 17, 'Eat': 18, 'F': 19, 'Friend': 20, 'G': 21, 'Go': 22, 'Good': 23, 'H': 24, 'Happy': 25, 'Help': 26, 'Home': 27, 'How': 28, 'I': 29, 'K': 30, 'L': 31, 'M': 32, 'N': 33, 'P': 34, 'Please': 35, 'Q': 36, 'R': 37, 'S': 38, 'Sad': 39, 'Small': 40, 'Sorry': 41, 'Stop': 42, 'T': 43, 'Teacher': 44, 'Thank you': 45, 'U': 46, 'V': 47, 'W': 48, 'Water': 49, 'What': 50, 'When': 51, 'Where': 52, 'Who': 53, 'X': 54, 'Y': 55, 'hello': 56, 'j': 57, 'why': 58, 'z': 59}  # Adjust as needed
 
 # Load the TFLite model
 interpreter = Interpreter(model_path="model.tflite")
@@ -55,3 +55,4 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
